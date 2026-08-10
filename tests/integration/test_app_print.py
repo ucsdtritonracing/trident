e6f6cccf47c5a4be0e6eaed5d4eaf970b2559094
Your Nameamrita.sivanesan2025@gmail.com <amrita.sivanesan2025@gmail.com>
2026-08-10 00:24:23 -0700

    Update filename for windows compatability

--- tests/integration/test_app_print.py
+++ tests/integration/test_app_print.py
@@ -4,7 +4,7 @@ import pytest
 
 
 repo_root = Path(__file__).resolve().parents[2]
-build_app_dir= repo_root / "build"/"host"/"embedded"/"app"
+build_app_dir = repo_root / "build" / "host" / "embedded" / "app"
 
 
 expected_output = {"front": ["Running on host!\nFront app running!"],
@@ -12,16 +12,19 @@ expected_output = {"front": ["Running on host!\nFront app running!"],
 
 
 def app_executable(app_name: str):
-    return build_app_dir / app_name /app_name
+    path_guess = build_app_dir / app_name / app_name
+    if path_guess.exists():
+        return path_guess
+    return build_app_dir / app_name / f"{app_name}.exe"
 
 @pytest.mark.parametrize("app_name",expected_output.keys())
 def test_app_print(app_name):
     executable_path = app_executable(app_name)
     assert executable_path.exists(),(
         f"Executable not found at {executable_path}. "
-        f"Did you run `just run {app_name}' before running the tests?"
+        f"Did you run `just build' before running the tests?"
     )
-    
+
     result = subprocess.run(
         [str(executable_path)],
         capture_output=True,
