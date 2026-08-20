81ebeb49004ccbd88cd4b679eda30d600b9f94f4
Your Nameamrita.sivanesan2025@gmail.com <amrita.sivanesan2025@gmail.com>
2026-08-19 21:12:30 -0700

    Fix formatting

--- tests/integration/test_app_print.py
+++ tests/integration/test_app_print.py
@@ -1,14 +1,16 @@
 import subprocess
 from pathlib import Path
-import pytest
 
+import pytest
 
 repo_root = Path(__file__).resolve().parents[2]
 build_app_dir = repo_root / "build" / "host" / "embedded" / "app"
 
 
-expected_output = {"front": ["Running on host!\nFront app running!"],
-                  "rear": ["Running on host!\nRear app running!"],}
+expected_output = {
+    "front": ["Running on host!\nFront app running!"],
+    "rear": ["Running on host!\nRear app running!"],
+}
 
 
 def app_executable(app_name: str):
@@ -17,10 +19,11 @@ def app_executable(app_name: str):
         return path_guess
     return build_app_dir / app_name / f"{app_name}.exe"
 
-@pytest.mark.parametrize("app_name",expected_output.keys())
+
+@pytest.mark.parametrize("app_name", expected_output.keys())
 def test_app_print(app_name):
     executable_path = app_executable(app_name)
-    assert executable_path.exists(),(
+    assert executable_path.exists(), (
         f"Executable not found at {executable_path}. "
         f"Did you run `just build' before running the tests?"
     )
@@ -38,6 +41,5 @@ def test_app_print(app_name):
     )
     for output in expected_output[app_name]:
         assert output in result.stdout, (
-            f"Expected line {output!r} not found in {app_name} stdout:\n"
-            f"{result.stdout!r}"
+            f"Expected line {output!r} not found in {app_name} stdout:\n{result.stdout!r}"
         )
