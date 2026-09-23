519dbd15f7687dede69439d0aac32c9832a490e1
nshie <njshie@gmail.com>
2026-09-23 00:51:53 -0700

    Add comments to CAN methods

--- embedded/platform/can/can.h
+++ embedded/platform/can/can.h
@@ -32,8 +32,28 @@ struct Message {
 class Peripheral {
 public:
     ~Peripheral() = default;
+
+    /**
+     * Send a CAN message over the peripheral.
+     *
+     * @param message The CAN message to send.
+     * @return The status of the send operation.
+     */
     virtual SendStatus Send(const Message& message) = 0;
+
+    /**
+     * Poll the peripheral for incoming CAN messages.
+     *
+     * @param out The buffer to store the received message.
+     * @return The status of the poll operation.
+     */
     virtual PollStatus Poll(Message& out) = 0;
+
+    /**
+     * Get the current bus state of the peripheral.
+     *
+     * @return The bus state.
+     */
     virtual BusState GetStatus() = 0;
 };
 
